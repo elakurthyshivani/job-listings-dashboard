d606e0d8c88b051aa63e957435a3a6a0ff86ccf6
elakurthyshivani <elakurthyshivani@gmail.com>
2024-01-23 23:44:02 -0500

    Added new daatabase name in Enum backend

--- job-listings-backend/Enum.py
+++ job-listings-backend/Enum.py
@@ -3,6 +3,7 @@ from enum import Enum
 class CosmosContainers(Enum):
     WORKDAY_JOB_LISTINGS = "workday-job-listings"
     GROUP_1_JOB_LISTINGS = "group-1-job-listings"
+    OWN_SITE_JOB_LISTINGS = "own-site-job-listings"
     CONTAINER_NAMES = "container-names"
 
 class JobStatus(Enum):
